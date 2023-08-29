22602f851301b05fe7949cb82e0cf934c32cb2a4
TakrayoutMohamed <takrayoutm@gmail.com>
2023-08-29 22:59:26 +0100

    update the main to execute the commands

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ CC=cc
 CFLAGS=-Wall -Werror -Wextra
 RM=rm -f
 HEADERS=./libglobalminishell.h
-SRC= convert_parsing_lst_to_execution.c
+SRC= convert_parsing_lst_to_execution.c convert_parsing_env_to_execution.c
 MAIN_SRC=main.c
 
 OBJ=$(SRC:.c=.o)
@@ -18,8 +18,8 @@ READLINE_LIB = -lreadline -L/goinfre/$(USER)/.brew/opt/readline/lib
 
 all:$(NAME)
 
-$(NAME) : $(MAIN_OBJ) $(LIBFT) $(LIBPARS) $(LIBMINI)
-		$(CC)  -o $@ $(MAIN_OBJ) $(LIBFT) $(LIBPARS) $(LIBMINI) $(READLINE_LIB)
+$(NAME) :$(OBJ) $(MAIN_OBJ) $(LIBFT) $(LIBPARS) $(LIBMINI)
+		$(CC)  -o $@ $(MAIN_OBJ) $(OBJ) $(LIBFT) $(LIBPARS) $(LIBMINI) $(READLINE_LIB)
 		@echo "the executable $@ has been created"
 
 
@@ -35,7 +35,7 @@ $(LIBMINI):
 		make -C ./execution all
 
 clean:
-	$(RM) $(MAIN_OBJ) $(LIBFT) $(LIBPARS) $(LIBMINI)
+	$(RM) $(MAIN_OBJ) $(LIBFT) $(OBJ) $(LIBPARS) $(LIBMINI)
 	make -C ./execution clean
 	make -C ./parsing clean
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 18:45:19 by oredoine          #+#    #+#             */
-/*   Updated: 2023/08/29 19:30:42 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/08/29 22:46:30 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,27 @@
 
 int main(int ac, char **av, char **env)
 {
+    t_llist     *env_list;
+    t_llist     *pars_llst;
+    t_list      *lst;
+    t_list      *env_lst;
+    char        *cmd_line;
+
     (void)ac;
     (void)av;
-    // (void)env;
-    // t_tokenize  *token;
-    t_llist     *env_list;
-    t_llist     *iterator;
-    char *cmd_line;
-
     while (1)
     {
         cmd_line = readline("\033[32m➜ minishell$ \033[0m");
         if (!cmd_line)
-            exit(0);
+            exit(EXIT_FAILURE);
     	if (cmd_line && ft_strlen(cmd_line) > 0)
 		{
 			add_history(cmd_line);
-			// added this to get the parsed data in type of t_llist
-			iterator = parse_data(cmd_line, env, &env_list);
+			pars_llst = parse_data(cmd_line, env, &env_list);
+            lst = convert_parsing_lst_to_execution(pars_llst);
+            env_lst = convert_parsing_env_to_execution(env_list);
+            execute_list(lst, env_lst);
 		}
-        // t_llist *tmpe = iterator;
-        // while (tmpe)
-        // {
-        //     token = (t_tokenize *)iterator->content;
-        //     token = tmpe->content;
-        //     printf("token->value : %s\n", token->value);
-        //     tmpe = tmpe->next;
-        // }
-        // **********************print list**************************
-        if(iterator)
-        {
-            t_llist *iterate = iterator;
-            while (iterate)
-            {
-                t_pass *cmde = (t_pass *)iterate->content;
-                int i = 0;
-                while (cmde->argument_cmd[i])
-                {
-                    printf("arg : %s\n",cmde->argument_cmd[i]);
-                    i++;
-                }
-                printf("=========\n");
-                iterate = iterate->next;
-            }
-        }
-        // **********************print list**************************
-        // void execution(t_llst *lst); hnaaaaaaaaaaaaaa fin atbda khdmtk; l head dyal list hwa iterator
-        ft_lstclearp(&iterator);
-        // free(cmd_line);
     }
+    ft_lstclearp(&pars_llst);
 }
-/*env */
-// t_llist *parse_data(char *str, env)
