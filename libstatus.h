470c7d6b93fdc91d9c71905ef33e47422b57092a
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-10 16:30:32 +0100

    the last changes

--- libstatus.h
+++ libstatus.h
@@ -6,13 +6,15 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 04:45:08 by takra             #+#    #+#             */
-/*   Updated: 2023/09/10 15:41:22 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/09/10 15:58:43 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBSTATUS_H
 # define LIBSTATUS_H
-# include "./libstructs.h"
+# ifndef LIBSTRUCTS_H
+#  include "./libstructs.h"
+# endif
 
 struct s_stats
 {
