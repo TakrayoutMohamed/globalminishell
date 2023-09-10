bd8f01c55fecaf9c7aad35c3703abdafed963dd6
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-10 15:45:19 +0100

    update libstruct libstatus convert execution env to parsing

--- convert_execution_env_to_parsing.c
+++ convert_execution_env_to_parsing.c
@@ -6,7 +6,7 @@
 /*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:53:02 by mohtakra          #+#    #+#             */
-/*   Updated: 2023/09/09 19:39:50 by mohtakra         ###   ########.fr       */
+/*   Updated: 2023/09/10 15:35:56 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ t_llist	*convert_execution_env_to_parsing(t_list *lst)
 		new = (t_env *) my_malloc(sizeof(t_env));
 		if (new == NULL)
 			return (NULL);
-		((t_env *)new)->key = ft_strdup(lst->key);
+		((t_env *)new)->key = ft_strdupp(lst->key);
 		if (lst->value != NULL)
-			value = ft_strdup(lst->value);
+			value = ft_strdupp(lst->value);
 		((t_env *)new)->value = value;
 		llst_tmp = ft_lstnewp(new);
 		ft_lstadd_backp(&llst, llst_tmp);
--- libstatus.h
+++ libstatus.h
@@ -3,21 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   libstatus.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mohtakra <mohtakra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 04:45:08 by takra             #+#    #+#             */
-/*   Updated: 2023/09/10 02:21:28 by takra            ###   ########.fr       */
+/*   Updated: 2023/09/10 15:41:22 by mohtakra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBSTATUS_H
 # define LIBSTATUS_H
+# include "./libstructs.h"
 
 struct s_stats
 {
-	int	status;
-	int	here_doc_stat;
-	int	flag_sigint;
-	int	delimiter_expand;
+	int		status;
+	int		here_doc_stat;
+	int		flag_sigint;
+	int		delimiter_expand;
+	t_llist	*garbage;
 } t_stats;
+
 #endif
