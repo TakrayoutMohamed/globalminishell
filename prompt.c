595b85cdbb7020c864ed6bfd1a7d5a3873833b40
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-14 06:19:08 +0100

    update about the commands to allow to be executed

--- prompt.c
+++ prompt.c
@@ -6,11 +6,33 @@
 /*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:51:12 by mohtakra          #+#    #+#             */
-/*   Updated: 2023/09/13 03:26:58 by takra            ###   ########.fr       */
+/*   Updated: 2023/09/14 06:18:13 by takra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libglobalminishell.h"
+
+// void print_llst(t_llist *iterator)
+// {
+// 	t_llist *iterate = iterator;
+// 	while (iterate)
+// 	{
+// 		// printf("here\n");
+// 		t_pass *cmde = (t_pass *)iterate->content;
+// 		int i = 0;
+// 		if (cmde->argument_cmd)
+// 		{
+// 			while (cmde->argument_cmd[i])
+// 			{
+// 				printf("arg : %s\n",cmde->argument_cmd[i]);
+// 				i++;
+// 			}
+// 		}
+// 		printf("infile =|%d|, outfile = |%d|\n", cmde->infile, cmde->outfile);
+// 		printf("=========\n");
+// 		iterate = iterate->next;
+// 	}
+// }
 
 static void	clean_data_lst(t_list *lst)
 {
@@ -30,7 +52,6 @@ void	prompt(t_list **env_lst, t_llist *env_list)
 	char	*cmd_line;
 	t_llist	*pars_llst;
 	t_list	*lst;
-	// int		currentstatus;
 
 	cmd_line = readline("\033[32m➜ minishell$ \033[0m");
 	if (!cmd_line)
@@ -44,13 +65,12 @@ void	prompt(t_list **env_lst, t_llist *env_list)
 	{
 
 		add_history(cmd_line);
-		// currentstatus = t_stats.status;
 		t_stats.here_doc_stat = 1;
 		pars_llst = parse_data(cmd_line, env_list);
 		free(cmd_line);
 		lst = convert_parsing_lst_to_execution(pars_llst);
 		ft_lstclearp(&pars_llst);
-		if (t_stats.here_doc_stat == 1)
+		if (t_stats.here_doc_stat == 1 && lst != NULL && lst->cmd != NULL)
 			execute_list(lst, env_lst);
 		clean_data_lst(lst);
 	}
